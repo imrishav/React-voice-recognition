d7397094bf3208f3364f3d0f9cd189faae431c32
Rishav Sinha <rishav@live.co.uk>
2020-06-18 04:02:19 +0530

    Netlfiy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { isChrome } from 'react-device-detect';
 
 import Colors from './components/Colors';
 import Header from './components/Header';
@@ -9,15 +10,34 @@ import { GlobalState } from './context/GlobalState';
 import './App.css';
 
 function App() {
-  return (
-    <GlobalState>
-      <Wrapper>
-        <Header />
-        <Speech />
-        <Colors />
-      </Wrapper>
-    </GlobalState>
-  );
+  if (!isChrome) {
+    return (
+      <GlobalState>
+        <Wrapper>
+          <Header />
+          <Speech />
+          <Colors />
+        </Wrapper>
+      </GlobalState>
+    );
+  } else {
+    return (
+      <div
+        style={{
+          backgroundColor: 'green',
+          width: 600,
+          height: 200,
+          margin: '0 auto',
+        }}
+      >
+        <h1 style={{ color: 'antiquewhite' }}>Chrome Only!!!</h1>
+        <p>
+          The Voice Recogination is Only Available for Chrome.Please Choose
+          Chrome.
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
