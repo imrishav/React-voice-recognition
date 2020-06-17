9e92c129389fce46532fadacdade4d6f73aec10f
Rishav Sinha <rishav@live.co.uk>
2020-06-18 03:18:16 +0530

    Minor Changes

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -6,15 +6,7 @@ import { colors } from '../utils/colors';
 import { ColorList } from './ColorList';
 
 const Colors = ({ children }) => {
-  // const [colors1, setColors] = useState([]);
-
-  // const colorArray = Object.keys(colors).sort((a, b) => a.length - b.length);
-  // console.log('colors', colorArray);
-  // useEffect(() => {
-  //   setColors(colorArray);
-  // }, []);
   const context = useContext(GlobalContext);
-  console.log('context ssss', context.color);
 
   return <div style={{ backgroundColor: context.color }}>{children}</div>;
 };
